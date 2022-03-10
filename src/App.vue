89f8a778505352417b12a380ef0cc8281d7ab17c
Daniele Galvão <daniele-fg@hotmail.com>
2022-03-09 22:00:07 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <Header class="mb-5" />
+    <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+      
+    <b-container>
+   
+    <b-row align-v="center">
+
+         <card v-for="curso in cursos" :key="curso.id" :nome="curso.nome"></card>  
+         
+    </b-row>    
+  </b-container>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import Card from '@/components/Card.vue';
+
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      cursos: []
+    }
+  },
+  components: {
+    Card,
+  },
+  mounted() {
+      this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      const res = await fetch("cursos.json");
+      const val = await res.json();
+      this.cursos = val;
+      console.log(val);
+    }
+  },
+}
+</script>
